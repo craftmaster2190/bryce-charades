4242e46ad9c0ae868075cc299c178b517a4e3889
Bryce J. Fisher <bryce.fisher@gmail.com>
2020-01-26 01:17:10 -0700

    WIP Adding Lists

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,11 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { LobbyComponent } from './lobby/lobby.component';
 import { GameComponent } from './game/game.component';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [AppComponent, LobbyComponent, GameComponent],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [BrowserModule, AppRoutingModule, NgbModule.forRoot()],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/lobby/lobby.component.ts
+++ src/app/lobby/lobby.component.ts
@@ -1,7 +1,9 @@
 import { GyroscopeService } from '../motion/gyroscope.service';
 import { VerticalDirectionService } from '../motion/vertical-direction.service';
+import { WordListsService } from '../game/word-lists.service';
 import { Component, OnInit } from '@angular/core';
 import { interval } from 'rxjs';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 import { throttle } from 'rxjs/operators';
 @Component({
   selector: 'app-lobby',
@@ -10,10 +12,23 @@ import { throttle } from 'rxjs/operators';
 })
 export class LobbyComponent implements OnInit {
   public notSupported = 'Loading Gyroscope...';
+  public currentWordList: string;
+  public addWord: string;
 
-  constructor(private readonly gyroscope: GyroscopeService) {}
+  constructor(
+    private readonly gyroscope: GyroscopeService,
+    public readonly wordLists: WordListsService,
+    private modalService: NgbModal
+  ) {}
 
   ngOnInit() {
     this.notSupported = this.gyroscope.notSupportedMessage;
+  }
+
+  addListClick(content) {
+    this.modalService
+      .open(content)
+      .result.then(() => {})
+      .catch(() => {});
   }
 }
